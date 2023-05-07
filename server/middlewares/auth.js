9e9aa5b9ec403b00234660396946a6accce6eb3f
A-ALAGH <drlaghoub@gmail.com>
2023-05-07 09:03:08 +0100

    importation in userRoutes Corrected

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,63 +1,35 @@
 const jwt = require("jsonwebtoken");
-const userModel = require("../Models/user");
-const generateToken = (id, email) => {
-  const token = jwt.sign({ id, email }, process.env.JWT_SECRET, {
-    expiresIn: process.env.TOKEN_EXPIRE_TIME,
-  });
-  return token.toString();
-};
+const userModel = require("../models/userModel");
 
-const verifyToken = async(req, res, next) => {
+const authmiddleware = async (req, res, next) => {
   try {
-    if (!req.headers["authorization"])
-      return res
-        .status(401)
-        .send({ errMessage: "Authorization token not found!" });
-
-    const header = req.headers["authorization"];
-    const token = header.split(" ")[1];
-
-    await jwt.verify(token, process.env.JWT_SECRET, async(err, verifiedToken) => {
-      if (err)
-        return res
-          .status(401)
-          .send({ errMessage: "Authorization token invalid", details: err });
-      const user = await userModel.findById(verifiedToken.id);
-      req.user = user;
-      next();
-    });
-  } catch (error) {
-    return res
-      .status(500)
-      .send({
-        errMesage: "Internal server error occured!",
-        details: error.message,
-      });
-  }
-};
-const authMiddleware = (req, res, next) => {
     // Récupération du token d'authentification depuis les headers de la requête
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-  
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
     // Vérification si le token existe
     if (!token) {
-      return res.status(401).json({ message: 'Token d\'authentification manquant' });
+      return res
+        .status(401)
+        .json({ message: "Token d'authentification manquant" });
     }
-  
-    try {
-      // Vérification de la validité du token
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      req.userId = decodedToken.userId;
-      next();
-    } catch (error) {
-      res.status(401).json({ message: 'Token d\'authentification invalide' });
-    }
-  };
-  
 
-module.exports = {
-  generateToken,
-  verifyToken,
-  authMiddleware
+    // Vérification de la validité du token
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decodedToken.id);
+
+    if (!user) {
+      return res.status(401).json({ message: "Utilisateur non trouvé" });
+    }
+
+    // Ajout de l'ID de l'utilisateur dans l'objet `req` pour être utilisé dans les routes protégées
+    req.userId = decodedToken.id;
+    next();
+  } catch (error) {
+    res
+      .status(401)
+      .json({ message: "Token d'authentification invalide", error: error });
+  }
 };
+
+module.exports = authmiddleware;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-
+const authMiddleware = require("./middlewares/auth");
 const app = express();
 
 // Configuration du parser pour les requêtes avec le body en json
@@ -19,11 +19,10 @@ mongoose.connect(process.env.DB_URI, {
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // Middleware pour l'authentification
-const authMiddleware = require('./middlewares/auth');
-app.use('/', authMiddleware);
+// app.use('/', authMiddleware);
 
 // Définition des routes pour les utilisateurs
-const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/userRoute');
 app.use('/api/users', userRoutes);
 
 // Démarrage du serveur
